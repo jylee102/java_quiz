139f2ce81e137c62bbe39c03745874a4a0056b1e
jylee102 <lilydodo11@gmail.com>
2024-02-15 12:15:44 +0900

    풀이

--- src/ch04/Quiz10.java
+++ src/ch04/Quiz10.java
@@ -5,40 +5,34 @@ import java.util.Scanner;
 public class Quiz10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int inputMenu = 0;
-        int remain = 0;
+        int balance = 0;
+        boolean run = true;
 
-        Label:
-        while (true) {
+        while (run) {
             System.out.println("----------------------");
             System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
             System.out.println("----------------------");
-
             System.out.print("선택> ");
-            inputMenu = scanner.nextInt();
 
-            switch (inputMenu) {
+            // 사용자로부터 입력을 받는다.
+            int menuNum = scanner.nextInt();
+
+            switch (menuNum) {
                 case 1 -> {
                     System.out.print("예금액 > ");
-                    int in = scanner.nextInt();
-
-                    remain += in;
+                    balance += scanner.nextInt(); // 통장 잔고에 기존에 예금했던 돈을 유지하기 위해 누적 합산
                 }
 
                 case 2 -> {
                     System.out.print("출금액 > ");
-                    int out = scanner.nextInt();
-
-                    remain -= out;
+                    balance -= scanner.nextInt();
                 }
 
-                case 3 -> System.out.println("잔고 > " + remain);
+                case 3 -> System.out.println("잔고 > " + balance);
 
-                default -> {
-                    System.out.println("\n프로그램 종료");
-                    break Label;
-                }
+                case 4 -> run = false;
             }
         }
+        System.out.println("\n프로그램 종료");
     }
 }
